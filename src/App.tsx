abf405025159f9e4ee19686c38ce6e28d26f8698
Vincent WENDLING <vincent@starling-burgers.com>
2022-06-09 14:25:08 +0200

    :construction: added the menu (still no content)

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,32 @@ function App() {
   const [cart, setCart]:any = useState([])
   const [orderNum, setOrderNum]:any = useState(1)
 
-  return (
-    <div className="App h-screen flex flex-col">
-      <div className="flex gap-2 p-2 h-full">
-        <Menu cart={cart} setCart={setCart}/>
-        <Cart cart={cart} setCart={setCart}/>
-      </div>
-      <div className="text-white h-10">
-        <div>
-          
-        </div>
-        <div>
+  const [pageSelected, setPageSelected]:any = useState("caisse")
 
-        </div>
+  const getCurrentPage = ()=> {
+      if (pageSelected === 'caisse') {
+        return (
+          <div className="flex gap-2 h-full">
+            <Menu cart={cart} setCart={setCart}/>
+            <Cart cart={cart} setCart={setCart}/>
+          </div>
+        )
+      }
+      else if (pageSelected === 'menu') {
+        return (
+          <div className="h-full w-full bg-background text-on-background">
+            TEXT
+          </div>
+        )
+      }
+  }
+
+  return (
+    <div className="App h-screen flex flex-col p-2">
+      { getCurrentPage() }
+      <div className="text-white h-10 w-full flex gap-2 pl-2 pr-2">
+          <button className="w-1/2 rounded-lg bg-secondary text-on-secondary" onClick={()=>setPageSelected('caisse')}>Caisse</button>
+          <button className="w-1/2 rounded-lg bg-secondary text-on-secondary" onClick={()=>setPageSelected('menu')}>Menu</button>
       </div>
     </div>
   )
